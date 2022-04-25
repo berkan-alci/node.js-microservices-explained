eb36b65744309f246e4613aaff6325b83a292aa2
berkan-alci <78726455+berkan-alci@users.noreply.github.com>
2022-04-25 03:44:49 +0200

    initial post dependency setup

--- posts/server.js
+++ posts/server.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+
 const app = express();
 
 //in memory repository
 const posts = {};
+
+let corsOptions = { origin: "*" };
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
 app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+    posts[id] = {
+        id, title
+    };
 
+    res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => {
