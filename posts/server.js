2a0716024f5fee8f48b3682e1a85effb5281cdb8
berkan-alci <78726455+berkan-alci@users.noreply.github.com>
2022-04-29 10:06:43 +0200

    address changes post/eb + addition of k8s config files

--- posts/server.js
+++ posts/server.js
@@ -23,8 +23,10 @@ app.post('/posts', async (req, res) => {
     posts[id] = {
         id, title
     };
-
-    await axios.post('http://localhost:4005/events', { type: 'PostCreated', data: { id, title } }).catch((err) => console.log(err.message));
+    //local
+    // await axios.post('http://localhost:4005/events', { type: 'PostCreated', data: { id, title } }).catch((err) => console.log(err.message));
+    //k8s
+    await axios.post('http://event-bus-srv:4005/events', { type: 'PostCreated', data: { id, title } }).catch((err) => console.log(err.message));
 
     res.status(201).send(posts[id]);
 });
@@ -37,5 +39,5 @@ app.post('/events', async (req, res) => {
 });
 
 app.listen(4000, () => {
-    console.log(`http://localhost:4000`);
+    console.log(`http://localhost:4000 k8s test`);
 });
